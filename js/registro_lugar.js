2b2f95e8edabaf19f2312bec1caac8571d52349d
unknown <galle2010@live.com.mx>
2017-12-04 09:51:01 -0600

    registro_lugar

---
+++ js/registro_lugar.js
@@ -0,0 +1,22 @@
+function registrolugar(lugar){
+console.log(lugar);
+  $.ajax({
+      url: "http://localhost:3000/api/lugares",
+      type: "POST",
+      crossDomain: true,
+      dataType: "JSON",
+      data: lugar,
+      success : function(response, status){
+        if(status === "success") {
+            alert("Lugar Registrado correctamente");
+            window.location.href="index.html";
+        } else {
+            alert("Error al Realizar el registro, Intentalo mas tarde");
+        }
+      },
+      error: function(error){
+          alert(error.responseJSON.error.message);
+      }
+    });
+
+}
