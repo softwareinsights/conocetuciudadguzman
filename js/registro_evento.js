41323f100a13be0b64d4858747ac754a7b886a54
unknown <galle2010@live.com.mx>
2017-12-04 10:44:18 -0600

    registro_evento

---
+++ js/registro_evento.js
@@ -0,0 +1,22 @@
+function registroevento(evento){
+console.log(evento);
+  $.ajax({
+      url: "http://localhost:3000/api/eventos",
+      type: "POST",
+      crossDomain: true,
+      dataType: "JSON",
+      data: evento,
+      success : function(response, status){
+        if(status === "success") {
+            alert("Evento Registrado correctamente");
+            window.location.href="index.html";
+        } else {
+            alert("Error al Realizar el registro, Intentalo mas tarde");
+        }
+      },
+      error: function(error){
+          alert(error.responseJSON.error.message);
+      }
+    });
+
+}
